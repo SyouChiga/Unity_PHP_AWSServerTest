4b7ba2f9f3d521735c0a4ec290a137e56085cd28
ChigaSho <tiga.syou.as@gmail.com>
2020-02-18 21:56:13 +0900

    PlayerClass_Add laravelConnection_False

---
+++ AWS_ServerTest/Assets/Script/Actor/Player/PlayerActor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    namespace Actor
+    {
+        //プレイヤークラス
+        public class PlayerActor : MonoBehaviour
+        {
+            //プレイヤーの操作クラス
+            [SerializeField]
+            Manual.PlayerManual playerManual_ = new Manual.PlayerManual();
+            // Start is called before the first frame update
+            void Start()
+            {
+
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                playerManual_.UpdateManual(gameObject.transform) ;
+            }
+        }
+    }
+}
---
+++ AWS_ServerTest/Assets/Script/Actor/Player/PlayerManual.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    namespace Manual
+    {
+        //プレイヤー操作クラス
+        public class PlayerManual
+        {
+            //移動値
+            const float move_ = 1.0f;
+
+            public void UpdateManual(Transform _transform)
+            {
+                Transform transform = _transform;
+
+                //座標を取得
+                Vector3 position = transform.position;
+
+                //物理取得
+                Rigidbody rigidbody = transform.gameObject.GetComponent<Rigidbody>();
+
+                //右に移動
+                if(Input.GetKey(KeyCode.A))
+                {
+                    rigidbody.AddForce(new Vector3(move_,0.0f,0.0f),ForceMode.Impulse);
+                }
+                //左に移動
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    rigidbody.AddForce(new Vector3(-move_, 0.0f, 0.0f), ForceMode.Impulse);
+                }
+
+            }
+        }
+    }
+}
